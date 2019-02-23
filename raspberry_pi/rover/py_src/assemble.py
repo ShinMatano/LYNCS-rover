9ab5bcd7c1c2c67abd56b2ae7c34bfa565420566
Shinichiro Matano <smatano@ShinichironoMacBook-Air.local>
2019-02-23 16:15:04 +0900

    Csearch1に戻り値を追加 assembleを完成

--- raspberry_pi/rover/py_src/assemble.py
+++ raspberry_pi/rover/py_src/assemble.py
@@ -3,15 +3,19 @@ import rover_module as gps
 from time import sleep
 
 cs = lyncs_rover.arduino_control()
-cs.Init()
+if cs.Init() == -1:
+    print('error')
+    exit()
 while True:
-    for i in range(25):
-        cs.Csearch1()
-
     length, theta = gps.r_theta_to_goal(35.555225, 139.654664)
+    for i in range(25):
+        judge=cs.Csearch1()
+        if length*1000 < 20 and judge == 1:
+            cs.Csearch2()
     #if r_theata[0]*1000 < 20:
     #    cs.Csearch2()
     #else:
-    cs.Transfer(int(theta*1000), 5)
+    if length*1000 >= 20:
+        cs.Transfer(int(theta*1000), 5)
     #print("s")
     #sleep(1)
