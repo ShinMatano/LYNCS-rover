58458454605262b14498b7d766f8da4ad9c7d966
Shinichiro Matano <smatano@ShinichironoMacBook-Air.local>
2019-02-23 17:59:29 +0900

    実験のため、カメラ発動のエリアを半径40mに変更

--- raspberry_pi/rover/py_src/assemble.py
+++ raspberry_pi/rover/py_src/assemble.py
@@ -10,12 +10,12 @@ while True:
     length, theta = gps.r_theta_to_goal(35.555388, 139.655796)
     for i in range(25):
         judge=cs.Csearch1()
-        if length*1000 < 20 and judge == 1:
+        if length*1000 < 40 and judge == 1:
             cs.Csearch2()
     #if r_theata[0]*1000 < 20:
     #    cs.Csearch2()
     #else:
-    if length*1000>=20:
+    if length*1000>=40:
         cs.Transfer(int(theta*1000), 5)
         print(int(theta*1000))
         print('5')
