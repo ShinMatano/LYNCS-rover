752008a96b10389b508cf3da125457cda8adab86
Shinichiro Matano <smatano@ShinichironoAir.lan>
2019-03-02 09:31:57 +0900

    実験用領域に変更

--- raspberry_pi/rover/py_src/assemble.py
+++ raspberry_pi/rover/py_src/assemble.py
@@ -18,12 +18,12 @@ while True:
     length, theta = gps.r_theta_to_goal(35.555355, 139.655751)
     for i in range(25):
         judge=cs.Csearch1()
-        if length*1000 < 10 and judge == 1:
+        if length*1000 < 100 and judge == 1:
             cs.Csearch2()
     #if r_theata[0]*1000 < 20:
     #    cs.Csearch2()
     #else:
-    if length*1000>=10:
+    if length*1000>=100:
         cs.Transfer(int(theta*1000), 5)
         print(int(theta*1000))
         print('5')
