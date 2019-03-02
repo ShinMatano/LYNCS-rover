dc961af68d68d95e5154abeb043e85aced79007b
Shinichiro Matano <smatano@ShinichironoAir.lan>
2019-03-02 12:06:04 +0900

    GPSのみの実験

--- raspberry_pi/rover/py_src/assemble.py
+++ raspberry_pi/rover/py_src/assemble.py
@@ -16,15 +16,15 @@ if cs.Init() == -1:
 #sleep(1)
 
 while True:
-    #length, theta = gps.r_theta_to_goal(35.555744, 139.654071)
-    #print(length)
-    #print(theta)
-    #cs.Transfer(int(theta*1000), 5)
-    #sleep(3)
+    length, theta = gps.r_theta_to_goal(35.555744, 139.654071)
+    print(length)
+    print(theta)
+    cs.Transfer(int(theta*1000), 5)
+    sleep(3)
     #for i in range(50):
         #judge=cs.Csearch1()
         #if length*1000 < 10000 and judge == 1:
-    cs.Csearch2()
+    #cs.Csearch2()
     #if r_theata[0]*1000 < 20:
     #    cs.Csearch2()
     #else:
